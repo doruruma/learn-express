e53b2f7caa7495c8241a7d8509cf26b3bc21dd40
doruruma <andra.yuda0308@gmail.com>
2022-10-15 21:12:38 +0700

    done practicing how workoutcontroller work'

---
+++ src/controllers/WorkoutController.js
@@ -0,0 +1,73 @@
+const Workout = require('../models/Workout')
+const mongoose = require('mongoose')
+
+// get all
+const getAll = async (req, res) => {
+    const workouts = await Workout.find({}).sort({ createdAt: -1 })
+    // response
+    res.status(200).json(workouts)
+}
+
+// get one
+const get = async (req, res) => {
+    const { id } = req.params
+    // check params id type
+    if (!mongoose.Types.ObjectId.isValid(id))
+        return res.status(204).json('')
+    // get workout
+    const workout = await Workout.findById(id)
+    if (!workout)
+        return res.status(204).json('')
+    // response
+    res.status(200).json(workout)
+}
+
+// store
+const store = async (req, res) => {
+    const { title, load, reps } = req.body
+    try {
+        // insert workout
+        const workout = await Workout.create({ title, load, reps })
+        res.status(200).json(workout)
+    } catch (error) {
+        // error
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// update
+const update = async (req, res) => {
+    const { id } = req.params
+    try {
+        // update workout
+        const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body })
+        if (!workout)
+            return res.status(204).json('')
+        res.status(200).json(workout)
+    } catch (error) {
+        // error
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// destroy
+const destroy = async (req, res) => {
+    const { id } = req.params
+    // check params id type
+    if (!mongoose.Types.ObjectId.isValid(id))
+        return res.status(204).json('')
+    // delete workout
+    const workout = Workout.findOneAndDelete({ _id: id })
+    if (!workout)
+        return res.status(204).json('')
+    // response
+    res.status(200).json(workout)
+}
+
+module.exports = {
+    getAll,
+    get,
+    store,
+    update,
+    destroy
+}
--- src/routes/workouts.js
+++ src/routes/workouts.js
@@ -1,28 +1,19 @@
 const router = require('express').Router()
+const { getAll, get, store, update, destroy } = require('../controllers/WorkoutController')
 
 // get all
-router.get('/', (req, res) => {
-  res.json({ title: 'workouts' })
-})
+router.get('/', getAll)
 
 // get one
-router.get('/:id', (req, res) => {
-  res.json({ title: 'one workout' })
-})
+router.get('/:id', get)
 
 // store workout
-router.post('/', (req, res) => {
-  res.json({ title: 'workout stored' })
-})
+router.post('/', store)
 
 // update workout
-router.patch('/:id', (req, res) => {
-  res.json({ title: 'workout updated' })
-})
+router.patch('/:id', update)
 
 // delete workout
-router.delete('/:id', (req, res) => {
-  res.json({ title: 'workout deleted' })
-})
+router.delete('/:id', destroy)
 
 module.exports = router
